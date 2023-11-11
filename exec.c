6f105608db7c4a8a12df7a6863b016bd0937edbb
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-11-11 19:53:57 +0200

    Updated

--- exec.c
+++ exec.c
@@ -6,10 +6,9 @@
 void exec(char *u_input)
 {
 	pid_t child_process;
-	char **argv;
 	int argc = 0, status;
-	char *text, *tmp_cmd = NULL, *last_cmd = NULL;
-	
+	char *text, **argv, *tmp_cmd = NULL, *last_cmd = NULL;
+
 	text = strtok(u_input, " ");
 	argv = malloc(sizeof(char *) * 1024);
 	while (text != NULL)
@@ -29,9 +28,8 @@ void exec(char *u_input)
 	{
 		tmp_cmd = argv[0];
 		last_cmd = search(tmp_cmd);
-		execve(last_cmd, argv, NULL);
-		perror("No such file or directory\n");
-		exit(EXIT_FAILURE);
+		if (execve(last_cmd, argv, NULL) == -1)
+                exit(0);
 	}
     else
     {
